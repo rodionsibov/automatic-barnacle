19d1eee41c748d45e100a49fb3e5091f9ae0e40c
Rodion Sibov <rodionsibov@gmail.com>
2022-03-08 23:35:42 +0200

    first commit

---
+++ src/pages/Details.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { ref } from "vue";
+
+const details = ref({
+  title: "Details",
+});
+
+const products = ref([
+  {
+    productTitle: "ABCN",
+    image: "https://picsum.photos/300?random=1",
+    productId: 1,
+  },
+  {
+    productTitle: "KARMA",
+    image: "https://picsum.photos/300?random=2",
+    productId: 2,
+  },
+  {
+    productTitle: "Tino",
+    image: "https://picsum.photos/300?random=3",
+    productId: 3,
+  },
+  {
+    productTitle: "EFG",
+    image: "https://picsum.photos/300?random=4",
+    productId: 4,
+  },
+  {
+    productTitle: "MLI",
+    image: "https://picsum.photos/300?random=5",
+    productId: 5,
+  },
+  {
+    productTitle: "Banans",
+    image: "https://picsum.photos/300?random=6",
+    productId: 6,
+  },
+]);
+</script>
+
+<template>
+  <div class="details">
+    <div class="container">
+      <h1 class="text-primary text-center">{{ details.title }}</h1>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
