268fdfdebc77980222e17aa585c25e87c676190e
Rodion Sibov <rodionsibov@gmail.com>
2022-03-15 18:23:16 +0200

    first commit

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,19 @@
+<script setup>
+</script>
+  
+<template>
+  <div class="sidebar">sidebar</div>
+</template>
+
+<style scoped>
+:root {
+  --sidebar-bg-color: #2f855a;
+  --sidebar-item-hover: #38a169;
+  --sidebar-item-active: #276749;
+}
+
+.sidebar {
+  color: white;
+  background-color: var(--sidebar-bg-color);
+}
+</style>
