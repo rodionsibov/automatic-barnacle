418e408084d11037727b3844c5182ab654a6a7a7
Rodion Sibov <rodionsibov@gmail.com>
2022-03-15 21:06:49 +0200

    first commit

---
+++ src/components/SidebarLinks.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+  to: {
+    type: String,
+    required: true,
+  },
+  icon: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+  
+<template></template>
+
+<style scoped>
+</style>
