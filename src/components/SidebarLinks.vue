973da56f5daa1e1af7ab1878762503f2e9bd30ca
Rodion Sibov <rodionsibov@gmail.com>
2022-03-16 21:13:54 +0200

    update

--- src/components/SidebarLinks.vue
+++ src/components/SidebarLinks.vue
@@ -33,4 +33,18 @@ const isActive = computed(() => route.path === props.to);
 </template>
 
 <style scoped>
+.link {
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  position: relative;
+  font-weight: 400;
+  user-select: none;
+  margin: 0.1em 0;
+  padding: 0.4em;
+  border-radius: 0.25em;
+  height: 1.5em;
+  color: white;
+  text-decoration: none;
+}
 </style>
